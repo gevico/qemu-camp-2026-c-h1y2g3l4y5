bc359b4461d559bb36cece42bd62ea46e6056b1f
laiguo-liang <2909269677@qq.com>
2026-04-17 19:48:53 +0800

    14

--- exercises/14_calculator/14_calculator.c
+++ exercises/14_calculator/14_calculator.c
@@ -3,12 +3,26 @@
 // TODO：定义四则运算规则
 // hint：似乎除数有一些特殊情况，需要处理
 
-// I AM NOT DONE
-int add(int a, int b) {  }
-int subtract(int a, int b) {  }
-int multiply(int a, int b) {  }
-int divide(int a, int b) {  }
 
+int add(int a, int b) {
+  return a + b;
+}
+int subtract(int a, int b) { 
+    return a - b;
+}
+int multiply(int a, int b) {  
+    return a * b;
+}
+int divide(int a, int b) { 
+    if(b==0){
+        printf("error\n");
+        return 0;
+    }
+        
+    return a / b;
+}
+
+typedef int (*Operation)(int, int);
 
 
 int main() {
@@ -17,7 +31,7 @@ int main() {
 
     // TODO
     // hint:这是一个函数指针数组，在这之前应该先定义指针
-    int ()() = {add, subtract, multiply, divide};
+    Operation operations[] = {add, subtract, multiply, divide};
 
     char operators[] = "+-*/";
 
