a0fb6a3648d8954eba160a52634185376a693d29
laiguo-liang <2909269677@qq.com>
2026-04-17 23:43:55 +0800

    17

--- exercises/17_myfile/17_myfile.c
+++ exercises/17_myfile/17_myfile.c
@@ -9,7 +9,12 @@ void print_elf_type(uint16_t e_type) {
   const char *type_str;
   switch (e_type) {
     // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+    case ET_NONE: type_str = "NONE (未知类型)"; break;
+    case ET_REL:  type_str = "REL (可重定位文件，通常是 .o 文件)"; break;
+    case ET_EXEC: type_str = "EXEC (可执行文件)"; break;
+    case ET_DYN:  type_str = "DYN (共享目标文件，如 .so 或 PIE 可执行文件)"; break;
+    case ET_CORE: type_str = "CORE (核心转储文件)"; break;
+    default:      type_str = "处理器特定或未知类型"; break;
   }
   printf("ELF Type: %s (0x%x)\n", type_str, e_type);
 }
@@ -24,7 +29,34 @@ int main(int argc, char *argv[]) {
   Elf64_Ehdr ehdr;
 
   // TODO: 在这里添加你的代码
-  // I AM NOT DONE
+  for (int i = 0; i < 2; i++) {
+    printf("正在检查文件: %s\n", filepath[i]);
+
+    // 以只读模式打开文件
+    fd = open(filepath[i], O_RDONLY);
+    if (fd < 0) {
+      perror("无法打开文件");
+      continue;
+    }
+
+    // 读取 ELF 头部 (位于文件最开始的部分)
+    if (read(fd, &ehdr, sizeof(Elf64_Ehdr)) != sizeof(Elf64_Ehdr)) {
+      perror("读取 ELF 头部失败");
+      close(fd);
+      continue;
+    }
+
+    // 基础验证：检查 ELF 魔数 (Magic Number)
+    // 前四个字节必须是 0x7f, 'E', 'L', 'F'
+    if (memcmp(ehdr.e_ident, ELFMAG, SELFMAG) == 0) {
+      print_elf_type(ehdr.e_type);
+    } else {
+      printf("错误: 不是一个有效的 ELF 文件。\n");
+    }
+
+    close(fd);
+    printf("---------------------------\n");
+  }
   
   return 0;
 }
