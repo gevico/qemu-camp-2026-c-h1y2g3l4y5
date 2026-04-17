aafa74658307b198e7ce88aec5fb0ab99a0291f8
laiguo-liang <2909269677@qq.com>
2026-04-17 16:55:03 +0800

    09

--- exercises/09_word_counter/09_word_counter.c
+++ exercises/09_word_counter/09_word_counter.c
@@ -1,10 +1,23 @@
 #include <stdio.h>
 
 int main() {
-    char str[]="Don't ask what your country can do for you, but ask what you can do for your country.";
+    //char str[]="Don't ask what your country can do for you, but ask what you can do for your country.";
+    char str[]=",,,....,,,.... ,,,..  ..... ..... ,,,,, .,  ,.";
     int wordCount = 0;
     // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+    int i = 0;
+    int inword = 0;
+    while(str[i] != '\0'){
+        if(str[i] == ' '){
+            inword = 0;
+        }else if((str[i] <= 'z' && str[i] >= 'a') || (str[i] <= 'Z' && str[i] >= 'A')){
+            if(inword == 0){
+                inword = 1;
+                wordCount++;
+            }
+        }
+        i++;
+    }
     
     printf("单词数量: %d\n", wordCount);
     
