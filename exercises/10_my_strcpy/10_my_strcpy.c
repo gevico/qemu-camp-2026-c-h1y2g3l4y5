40d13387e9c9159ed217b7041c6e024054d40500
laiguo-liang <2909269677@qq.com>
2026-04-17 17:32:44 +0800

    10

--- exercises/10_my_strcpy/10_my_strcpy.c
+++ exercises/10_my_strcpy/10_my_strcpy.c
@@ -5,7 +5,12 @@ int main() {
     char destination[101];
     
 	// TODO: 在这里添加你的代码
-    // I AM NOT DONE
+    int i = 0;
+    while(source[i]!='\0'){
+        destination[i] = source[i];
+        i++;
+    }
+    destination[i+1]='\0';
     
     printf("拷贝后的字符串: %s", destination);
     
